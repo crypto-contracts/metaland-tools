589347b72cf6f8969f49b78d593c2e78ab24ea79
Allan Wakes <allanwakes@gmail.com>
2022-02-22 13:58:49 +0800

    testnet ready

--- contracts/monster/emf_v2_auction.sol
+++ contracts/monster/emf_v2_auction.sol
@@ -2,28 +2,34 @@
 pragma solidity ^0.8.10;
 
 contract MineAuction {
-    event Bid(uint amount, uint tokenId);
+    event Bid(address bidder, uint amount, uint charType, uint tokenId);
 
     IERC20 public acceptableToken;
     IERC721 public mms2;
+    IERC721 public mms1;
+    IERC721 public hero;
 
-    uint public phrase;  // 1 - started/free bid, 2 - bid then extend
-    uint public phraseOneEndAt;
-    uint public endedAt;  // extend timestamp for last biding in phrase 2
-    bool public ended;
-    address public highestBidder;
+    uint public phrase;             // 1 - started/free bid, 2 - bid then extend
+    uint public phraseOneEndAt;     // when will the free bid end
+    uint public endedAt;            // when will the limited time bid end
+    bool public ended;              // note: flag to indicate if auction is ended, might not be valid if no one trigger its change via bidding
+    address public highestBidder;   // bid winner
     address public owner;
-    address public target;
+    address public target;          // final address where bidding fund will be transferred to
     uint public highestBid;
-    uint public bidInc;
-    uint public tokenId;
+    uint public bidInc;             // bidding amount increment
+    uint public tokenId;            // which char will get entry fee, used with charType, 1: hero, 2: mms1, 3: mms2
+    uint public charType;
     uint public phraseOneDelay;
     uint public phraseTwoDelay;
+    uint public round;
 
-    constructor(address _a, address _m, uint _startingBid, uint _bidInc) {
+    constructor(address _a, address _h, address _m1, address _m2, uint _startingBid, uint _bidInc) {
         owner = msg.sender;
         acceptableToken = IERC20(_a);
-        mms2 = IERC721(_m);
+        hero = IERC721(_h);
+        mms1 = IERC721(_m1);
+        mms2 = IERC721(_m2);
 
         highestBid = _startingBid;
         bidInc = _bidInc;
@@ -49,14 +55,22 @@ contract MineAuction {
     function bid(uint _charType, uint _tokenId, uint _amount) external {
         require(ended == false, "auc ended - ended");
         require(_amount >= highestBid + bidInc, "value < highest");
+
         if (_charType == 3) {
             if (msg.sender != mms2.ownerOf(_tokenId)) {
-                revert("");
+                revert("wrong NFT owner");
             }
-        } else {
-            //
+        } else if (_charType == 2) {
+            if (msg.sender != mms1.ownerOf(_tokenId)) {
+                revert("wrong NFT owner");
+            }
+        } else if (_charType == 1) {
+            if (msg.sender != hero.ownerOf(_tokenId)) {
+                revert("wrong NFT owner");
+            }
         }
-        require(msg.sender == mms2.ownerOf(_tokenId), "wrong ownner");  // todo: address cannot change tokenId
+
+        require(msg.sender == mms2.ownerOf(_tokenId), "wrong ownner");  // note: tokenId could be changed between biddings
 
         if (phrase == 0) {  // start and go into phrase 1
             phrase = 1;
@@ -66,13 +80,12 @@ contract MineAuction {
                 if (block.timestamp <= phraseOneEndAt + phraseTwoDelay) {  // if new bid time in oneendat and first twodelay, then enter 2 phrase
                     phrase = 2;
                     endedAt = block.timestamp + phraseTwoDelay;
-                } else {  // 
+                } else {  // time beyond oneendat and first twodelay, then auction ends at phrase 1
                     ended = true;
                     revert("auc ended - expired 1");
                 }
             }
         } else if (phrase == 2) {
-            // require(block.timestamp < endedAt, "auc ended - ended");
             if (block.timestamp > endedAt) {
                 ended = true;
                 revert("auc ended - expired 2");
@@ -84,45 +97,44 @@ contract MineAuction {
 
         acceptableToken.transferFrom(msg.sender, address(this), _amount);  // get this bid first
         if (highestBidder != address(0)) {
-            acceptableToken.transfer(highestBidder, highestBid);  // send back to last bidder
+            acceptableToken.transfer(highestBidder, highestBid);  // send back to last bidder if we have one
         }
 
         highestBidder = msg.sender;
         highestBid = _amount;
         tokenId = _tokenId;
+        charType = _charType;
+        round = round + 1;
 
-        // emit Bid(msg.sender, msg.value);
+        emit Bid(highestBidder, highestBid, charType, tokenId);
     }
 
-    function isAuctionEnd() public view returns (bool, uint, address, uint, uint) {  // pure observer mode
+    function isAuctionEnd() public view returns (bool) {  // observer which can tell actual status of this auction
         if (phrase == 1) {
             if (block.timestamp > phraseOneEndAt + phraseTwoDelay) {
-                return (true, phrase, highestBidder, highestBid, tokenId);
+                return true;
             }
         } else if (phrase == 2) {
             if (block.timestamp > endedAt) {
-                return (true, phrase, highestBidder, highestBid, tokenId);
+                return true;
             }
         }
 
-        return (false, phrase, highestBidder, highestBid, tokenId);
+        return false;
     }
 
     function setTarget(address _t) external onlyOwner validAddress(_t) {
         target = _t;
     }
 
+    function changeTokenId(uint _charType, uint _tokenId) external onlyOwner {
+        charType = _charType;
+        tokenId = _tokenId;
+    }
+
     function withdraw() external onlyOwner {
         require(target != address(0), "invalid target");
-        // require(ended == true, "auc not ended");
-        (bool auctionEnd, uint p, address hbr, uint hb, uint t) = isAuctionEnd();
-        if (auctionEnd == false) {
-            if (p == 0) {
-                revert("auc not started");
-            } else {
-                revert("auc still running");
-            }
-        }
+        require(isAuctionEnd(), "auc not ended");
 
         acceptableToken.transferFrom(address(this), target, highestBid);
     }
